bcd280ce762a9b72387dc749615f6632f3629a53
jerome jossent <jerome.jossent@veolia.com>
2022-02-05 22:34:04 +0100

    ColorPicker | UI

--- BW_to_WandAlpha/MainWindow.xaml.cs
+++ BW_to_WandAlpha/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using OpenCvSharp;
-using OpenCvSharp.Extensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
+//using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -20,8 +19,11 @@ namespace BW_to_WandAlpha
 {
     public partial class MainWindow : System.Windows.Window, INotifyPropertyChanged
     {
+        const string titre = "Black and White → White and Alpha (Transparent)";
         Mat source;
         Dictionary<string, Mat> mats;
+        System.Windows.Media.Color color = Color.FromRgb(0, 0, 0);
+        Mat colors;
 
         #region BINDING
         public event PropertyChangedEventHandler PropertyChanged;
@@ -34,43 +36,36 @@ namespace BW_to_WandAlpha
 
         public string _folder_IN
         {
-            get { return folder_IN; }
+            get { return Properties.Settings.Default["folder_IN"].ToString(); }
             set
             {
-                if (folder_IN != value)
-                {
-                    folder_IN = value;
-                    OnPropertyChanged("_folder_IN");
-                }
+                Properties.Settings.Default["folder_IN"] = value;
+                Properties.Settings.Default.Save();
+                OnPropertyChanged("_folder_IN");
             }
         }
-        string folder_IN = @"D:\C#\OpenCVSharpJJ\data";
 
         public string _folder_OUT
         {
-            get { return folder_OUT; }
+            get { return Properties.Settings.Default["folder_OUT"].ToString(); }
             set
             {
-                if (folder_OUT != value)
-                {
-                    folder_OUT = value;
-                    OnPropertyChanged("_folder_OUT");
-                }
+                Properties.Settings.Default["folder_OUT"] = value;
+                Properties.Settings.Default.Save();
+                OnPropertyChanged("_folder_OUT");
             }
         }
-        string folder_OUT = @"D:\C#\OpenCVSharpJJ\data2";
         #endregion
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
-            sld_b.Value = 255;
-            sld_g.Value = 0;
-            sld_r.Value = 255;
+            colors = new Mat("Images\\colorpicker.png");
+            _colorNew += MainWindow__colorNew; ;
         }
 
-        void btn_ReadDirectory_click(object sender, RoutedEventArgs e)
+        private void btn_ReadDirectory_click(object sender, MouseButtonEventArgs e)
         {
             mats = new Dictionary<string, Mat>();
 
@@ -86,7 +81,7 @@ namespace BW_to_WandAlpha
                 lb.Items.Add(newItem(mat, fichier));
                 //Application.Current.
                 //Dispatcher.BeginInvoke(new Action(() =>                    {
-                Title = lb.Items.Count + " / " + fichiers.Length;
+                Title = titre + " - " + lb.Items.Count + " / " + fichiers.Length;
                 //}));
                 System.Threading.Thread.Sleep(1);
             }
@@ -109,54 +104,21 @@ namespace BW_to_WandAlpha
             Mat gray = new Mat();
             Cv2.CvtColor(src, gray, ColorConversionCodes.RGB2GRAY);
 
-            int seuil1 = (int)filter.LowerValue;
-            int seuil2 = (int)filter.HigherValue;
-
-            //if (seuil1 > 0 || seuil2 < 255)
-            //{//passe bande : on ne garde que ce qui est au dessus de seuil1 et en dessous de seuil2:
-            //    //en travaux
-            //    gray = gray.Threshold(seuil1, seuil2, ThresholdTypes.Otsu | ThresholdTypes.Binary);
-            //}
-
             Mat gray_not = new Mat();
-            Cv2.BitwiseNot(gray, gray_not);
+            if (ckb_not_a.IsChecked == true)
+                Cv2.BitwiseNot(gray, gray_not);
 
-            Mat[] bgra = new Mat[] { Mat.Ones(src.Size(),MatType.CV_8UC1) * sld_b.Value,
-                                     Mat.Ones(src.Size(),MatType.CV_8UC1) * sld_g.Value,
-                                     Mat.Ones(src.Size(),MatType.CV_8UC1) * sld_r.Value,
+            Mat[] bgra = new Mat[] { Mat.Ones(src.Size(),MatType.CV_8UC1) * color.B,
+                                     Mat.Ones(src.Size(),MatType.CV_8UC1) * color.G,
+                                     Mat.Ones(src.Size(),MatType.CV_8UC1) * color.R,
                                      (ckb_not_a.IsChecked==true)? gray_not:gray};
-
-            //Mat[] bgra = new Mat[] { gray_not,
-            //                         gray_not,
-            //                         gray_not,
-            //                         gray_not};
-
-            //Mat[] bgra = new Mat[] { gray,
-            //                         gray,
-            //                         gray,
-            //                         gray_not};
 
             Mat newmat = new Mat();
             Cv2.Merge(bgra, newmat);
             return newmat;
         }
 
-        void slds_management(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            tb_b.Text = "Blue  : " + sld_b.Value;
-            tb_g.Text = "Green : " + sld_g.Value;
-            tb_r.Text = "Red   : " + sld_r.Value;
-            if (source != null)
-                ProcessOnSelectedItem();
-        }
-
         private void ckb_management(object sender, RoutedEventArgs e)
-        {
-            if (source != null)
-                ProcessOnSelectedItem();
-        }
-
-        void filter_values_Changed(object sender, RoutedEventArgs e)
         {
             if (source != null)
                 ProcessOnSelectedItem();
@@ -177,18 +139,84 @@ namespace BW_to_WandAlpha
             img_after.Source = conversion.ToImageSource(mat_out);
         }
 
-        void btn_SaveSelected_click(object sender, RoutedEventArgs e)
+        private void btn_SaveSelected_click(object sender, MouseButtonEventArgs e)
         {
-            ListBoxItem item = (ListBoxItem)lb.SelectedItem;
-            string path = item.ToolTip.ToString();
-            Mat mat = mats[path];
-            Mat mat_out = ImageProcessing(mat);
+            string msg;
+            try
+            {
+                ListBoxItem item = (ListBoxItem)lb.SelectedItem;
+                string path = item.ToolTip.ToString();
+                Mat mat = mats[path];
+                Mat mat_out = ImageProcessing(mat);
 
-            string filename = System.IO.Path.GetFileNameWithoutExtension(path);
-
-            mat_out.SaveImage(folder_OUT + "\\" + filename + ".png");
+                string filename = System.IO.Path.GetFileNameWithoutExtension(path);
+                string fullfilename = _folder_OUT + "\\" + filename + ".png";
+                mat_out.SaveImage(fullfilename);
+                msg = "File created : \n\n" + fullfilename;
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+            MessageBox.Show(msg);
         }
 
+        void Colors_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MouseMove += Colors_MouseMove;
+            MouseLeftButtonUp += Colors_MouseLeftButtonUp;
+        }
 
+        void Colors_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MouseMove -= Colors_MouseMove;
+            MouseLeftButtonUp -= Colors_MouseLeftButtonUp;
+        }
+
+        public class NewColorEventArgs : EventArgs
+        {
+            public Color color { get; private set; }
+            public NewColorEventArgs(Color color)
+            {
+                this.color = color;
+            }
+        }
+
+        public event EventHandler<NewColorEventArgs> _colorNew;
+
+        void Colors_MouseMove(object sender, MouseEventArgs e)
+        {
+            //relative position
+            System.Windows.Point ui_pos = e.GetPosition(_Colors);
+            int x = (int)(ui_pos.X / _ColorsGrid.ActualWidth * colors.Width);
+            int y = (int)(ui_pos.Y / _ColorsGrid.ActualHeight * colors.Height);
+
+            //get pixel
+            Vec3b px = colors.Get<Vec3b>(y, x);
+
+            //read pixel to make color
+            Color color = Color.FromArgb(255, px[2], px[1], px[0]);
+            _SetTarget(ui_pos);
+            this.color = color;
+            _colorNew?.Invoke(null, new NewColorEventArgs(color));
+        }
+
+        private void _SetTarget(System.Windows.Point ui_pos)
+        {
+            int d = 10;
+            double x = ui_pos.X;
+            double y = ui_pos.Y;
+            _lineNO.Points = new PointCollection(new List<System.Windows.Point> { new System.Windows.Point(x - d, y - 1), new System.Windows.Point(x - 1, y - 1), new System.Windows.Point(x - 1, y - d) });
+            _lineNE.Points = new PointCollection(new List<System.Windows.Point> { new System.Windows.Point(x + d, y - 1), new System.Windows.Point(x + 1, y - 1), new System.Windows.Point(x + 1, y - d) });
+            _lineSE.Points = new PointCollection(new List<System.Windows.Point> { new System.Windows.Point(x - d, y + 1), new System.Windows.Point(x - 1, y + 1), new System.Windows.Point(x - 1, y + d) });
+            _lineSO.Points = new PointCollection(new List<System.Windows.Point> { new System.Windows.Point(x + d, y + 1), new System.Windows.Point(x + 1, y + 1), new System.Windows.Point(x + 1, y + d) });
+        }
+
+        private void MainWindow__colorNew(object sender, NewColorEventArgs e)
+        {         
+            _colorRGB.Content = "RGB (" + e.color.R + ", " + e.color.G + ", " + e.color.B + ")";
+            _ColorPicker.Background = new SolidColorBrush(e.color);
+            ProcessOnSelectedItem();
+        }
     }
 }
--- BW_to_WandAlpha/Properties/Settings.Designer.cs
+++ BW_to_WandAlpha/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BW_to_WandAlpha.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,30 @@ namespace BW_to_WandAlpha.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string folder_OUT {
+            get {
+                return ((string)(this["folder_OUT"]));
+            }
+            set {
+                this["folder_OUT"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string folder_IN {
+            get {
+                return ((string)(this["folder_IN"]));
+            }
+            set {
+                this["folder_IN"] = value;
             }
         }
     }
